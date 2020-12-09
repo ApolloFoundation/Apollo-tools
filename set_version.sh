fa9870cefc652ffafb76c761ee289861936160b2
Andrew Boyarsky <andrew.boyarski@gmail.com>
2020-12-09 09:55:57 +0200

    Fix set_version.sh (remove Constants version updating), bump version to 1.48.0

--- set_version.sh
+++ set_version.sh
@@ -12,9 +12,6 @@ echo $NEW_VERSION > $VF
 echo "Changing Maven's POM files"
 ./mvnw versions:set -DnewVersion=${NEW_VERSION}
 
-# set verions in Constants.java (application hardcoded version)
-CONST_PATH=apl-utils/src/main/java/com/apollocurrency/aplwallet/apl/util/Constants.java
-echo "Changing Constants in $CONST_PATH"
 PKG_PATH=packaging/pkg-apollo-tools.json
 echo "Changing pkg-tools-blockchain.json"
 sed -i -e "s/\ \"version\".*/ \"version\": \"$NEW_VERSION\",/g" ${PKG_PATH}
